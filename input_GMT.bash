4ece8bbe499711486186a05e20f9856825101ecb
Rakesh <55206598+rakeshkstp@users.noreply.github.com>
2020-04-11 09:15:59 -0400

    Add files via upload

--- input_GMT.bash
+++ input_GMT.bash
@@ -13,13 +13,18 @@ L=-Lf32.5/77/10/100+lkm #Scale
 T=-Tdg8/80.2+w0.8c+f+l,,,N #Rose
 #landmask=-ESJ+gdarkgrey+p0.25p,black+r # Svalbard
 landmask="-Dh -G120 -W0.2p,black"
-cfont="gmt set FONT_ANNOT_PRIMARY 14p,Helvetica FONT_TITLE 10p MAP_TITLE_OFFSET 0.1c \
-        MAP_ANNOT_OFFSET_PRIMARY 0.1c MAP_ANNOT_OFFSET_SECONDARY 0.1c MAP_LABEL_OFFSET 0.2c"
-gmt set COLOR_NAN 200
+cfont="gmt set FONT_ANNOT_PRIMARY 14p,Helvetica FONT_TITLE 10p MAP_TITLE_OFFSET 0.1c MAP_ANNOT_OFFSET_PRIMARY 0.1c MAP_ANNOT_OFFSET_SECONDARY 0.1c MAP_LABEL_OFFSET 0.2c"
+gmt gmtset COLOR_NAN 200
 mfont="gmt set FONT_ANNOT_PRIMARY 20p,Helvetica \
         MAP_GRID_CROSS_SIZE_PRIMARY 0.1i MAP_ANNOT_OFFSET_PRIMARY 0.2c"
-chlscale="-Cjet -T0/1/0.1 -Z -Do -Qi"
+chlscale="-Cjet -T0/1/0.01 -Z -Do -Qi"
 spmscale="-Cdrywet -I -T0/1.35/0.1 -Z -Do -Qi"
 parbscale="-Cpanoply -T-2/1/0.1 -Z -Do -Qi"
-parsscale="-Cpanoply -T0/60/1 -Z -Do"
+parsscale="-Cpanoply -T0/60/0.1 -Z -Do"
 seaicescale="-Cblue,cyan,aquamarine,white -T0/1/0.1 -Z -Do"
+icwscale="-Cblue,aquamarine,white -T0/2/1 -Z -Do"
+COTscale="-Cpolar -T0/65/0.1 -Z -Do"
+OOTscale="-Ccool -T300/400/1 -Z -Do"
+salbscale="-Cwysiwyg -T0/0.85/0.01 -Z -Do"
+rhosscale="-Crainbow -T0/0.2/0.01 -Z -Do"
+statname="Median"
--- plot_GMT.bash
+++ plot_GMT.bash
@@ -16,54 +16,111 @@ for ncfile in `cat product.lst`
   do
 		$mfont
     IFS='_' read -r year month prod  <<< ${ncfile:3}
-  	if [[ ${prod:0:3} == "Chl" ]]; then
+  	if [[ ${prod:0:3} == "ABI" ]]; then
 			rm -f $scale
-			iprod="Chlorophyll.a"
-			product="Chl"
+			iprod="ABI_Chlorophyll.a"
+			product="ABI_Chl"
+			gmt makecpt $chlscale > $scale
+		elif [[ ${prod:0:3} == "GAB" ]]; then
+			rm -f $scale
+			iprod="GABI_Chlorophyll.a"
+			product="GABI_Chl"
 			gmt makecpt $chlscale > $scale
 		elif [[ ${prod:0:3} == "SPM" ]]; then
 			rm -f $scale
 			iprod="SPM"
 			product="SPM"
 			gmt makecpt $spmscale > $scale
-		elif [[ ${prod:0:4} == "PARb" ]]; then
+		elif [[ ${prod:0:8} == "PAR_OBPG" ]]; then
 			rm -f $scale
-			iprod="PARb"
-			product="PARb"
-			gmt makecpt $parbscale > $scale
-		elif [[ ${prod:0:4} == "PARs" ]]; then
-			rm -f $scale
-			iprod="PARs"
-			product="PARs"
+			iprod="PAR_OBPG"
+			product="PAR_OBPG"
 			gmt makecpt $parsscale > $scale
+		elif [[ ${prod:0:11} == "PAR_UQAR_0p" ]]; then
+			rm -f $scale
+			iprod="PAR_UQAR_0plus"
+			product="PAR_UQAR_0plus"
+			gmt makecpt $parsscale > $scale
+		elif [[ ${prod:0:11} == "PAR_UQAR_0m" ]]; then
+			rm -f $scale
+			iprod="PAR_UQAR_0minus"
+			product="PAR_UQAR_0minus"
+			gmt makecpt $parsscale > $scale
+		elif [[ ${prod:0:15} == "PAR_UQAR_bottom" ]]; then
+			rm -f $scale
+			iprod="PAR_UQAR_bottom"
+			product="PAR_UQAR_bottom"
+			gmt makecpt $parbscale > $scale
 		elif [[ ${prod:0:3} == "Sea" ]]; then
 			rm -f $scale
 			iprod="SeaIce"
 			product="SIF"
 			gmt makecpt $seaicescale > $scale
+		elif [[ ${prod:0:3} == "ICW" ]]; then
+			rm -f $scale
+			iprod="ICW"
+			product="ICW"
+			gmt makecpt $icwscale > $scale
+		elif [[ ${prod:0:3} == "COT" ]]; then
+			rm -f $scale
+			iprod="COT"
+			product="COT"
+			gmt makecpt $COTscale > $scale
+		elif [[ ${prod:0:3} == "OOT" ]]; then
+			rm -f $scale
+			iprod="OOT"
+			product="OOT"
+			gmt makecpt $OOTscale > $scale
+		elif [[ ${prod:0:4} == "salb" ]]; then
+			rm -f $scale
+			iprod="salb"
+			product="salb"
+			gmt makecpt $salbscale > $scale
+		elif [[ ${prod:0:4} == "rhos" ]]; then
+			rm -f $scale
+			iprod="rhos_2130"
+			product="rhos_2130"
+			gmt makecpt $rhosscale > $scale
 		else
 			echo "Not a valid image file."
 			continue
 		fi
-		nc="${ncfile}?Median_${iprod}_${year}_${month}"
-    ps="${product}_Median_${year}_${month}.ps"
-    gmt grdimage $nc -C$scale -t100 $R $J -K -E300i -G100 > $ps
+
+
+		if [[ ${prod:0:3} == "ICW" ]]; then
+			statname="Minimum"
+		fi
+		nc="${ncfile}?${statname}_${iprod}_${year}_${month}"
+    ps="${product}_${statname}_${year}_${month}.ps"
+
+
+		gmt grdimage $nc -C$scale -t100 $R $J -K -E300i -G100 > $ps
     gmt pscoast $R $J $lon $landmask -K -O >> $ps
     gmt pscoast $R $J $lat $landmask -K -O >> $ps
     $cfont
     gmt pscoast $R $J -Ba0g0fswne $landmask -K -O >> $ps
     #echo $loc1 "${month}, ${year}" | gmt pstext -R -J -O -K -F+f12p,Helvetica,white+jLB >> $ps
     #echo $loc2 "Chlorophyll-a" | gmt pstext -R -J -O -K -F+f12p,Helvetica,white+jLB >> $ps
-		if [[ ${prod:0:3} == "Chl" ]]; then
+		if [[ ${prod:0:3} == "ABI" ]]; then
+			gmt psscale -C$scale $D -Bxa+l"mg m@+-3@+" -S -O -N300i -t100 -Q >> $ps
+		elif [[ ${prod:0:3} == "GAB" ]]; then
 			gmt psscale -C$scale $D -Bxa+l"mg m@+-3@+" -S -O -N300i -t100 -Q >> $ps
 		elif [[ ${prod:0:3} == "SPM" ]]; then
 			gmt psscale -C$scale $D -Bx2+l"g m@+-3@+" -S -O -N300i -t100 -Q >> $ps
-		elif [[ ${prod:0:4} == "PARb" ]]; then
-			gmt psscale -C$scale $D -Bx0.5+l"E m@+-2@+ day@+-1@+" -S -O -N300i -t100 -Q >> $ps
-		elif [[ ${prod:0:4} == "PARs" ]]; then
+		elif [[ ${prod:0:3} == "PAR" ]]; then
 			gmt psscale -C$scale $D -Bxa+l"E m@+-2@+ day@+-1@+" -S -O -N300i -t100 >> $ps
 		elif [[ ${prod:0:3} == "Sea" ]]; then
 			gmt psscale -C$scale $D -Bxa+l"Sea Ice Fraction" -O -N300i -S -t100 >> $ps
+		elif [[ ${prod:0:3} == "ICW" ]]; then
+			gmt psscale -C$scale $D -Bx+l"Water     Ice     Cloud" -O -N300i -S -t100 >> $ps
+		elif [[ ${prod:0:3} == "COT" ]]; then
+			gmt psscale -C$scale $D -Bxa -S -O -N300i -t100 >> $ps
+		elif [[ ${prod:0:3} == "OOT" ]]; then
+			gmt psscale -C$scale $D -Bxa+l"DU" -S -O -N300i -t100 >> $ps
+		elif [[ ${prod:0:3} == "sal" ]]; then
+			gmt psscale -C$scale $D -Bxa -S -O -N300i -t100 >> $ps
+		elif [[ ${prod:0:3} == "rho" ]]; then
+			gmt psscale -C$scale $D -Bxa -S -O -N300i -t100 >> $ps
 		else
 			echo "Cannot create scale, invalid image file."
 			continue
@@ -75,4 +132,4 @@ for ncfile in `cat product.lst`
 	done
 rm -f product.lst
 rm -f gmt.conf
-mv *.png ./PNG
+#mv *.png ./PNG
